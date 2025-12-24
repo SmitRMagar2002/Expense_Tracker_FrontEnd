de574706869521cce06920e28f5247a1269bf8b2
smit raika magar <smitraikamagar@smits-MacBook-Air.local>
2025-12-24 17:40:04 -0600

    firstday

---
+++ src/app/post/[post]/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { useParams } from 'next/navigation';
+import React from 'react';
+
+export default function Page() {
+  const params = useParams();
+
+  return (
+    <div>This is my post {params.post}</div>
+  );
+}
